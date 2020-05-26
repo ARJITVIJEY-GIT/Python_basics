a253510b6797e87066e51c0bd3d0b9a67ade9ddf
ARJIT VIJEY <60552906+ARJITVIJEY-GIT@users.noreply.github.com>
2020-05-26 09:50:41 -0700

    Update tuplesandsets.py

--- tuplesandsets.py
+++ tuplesandsets.py
@@ -2,7 +2,7 @@
 tup = (3,8,9,5,9,8)
 tup.count(9)
 print(tup)
-#set,similar to list
+#set,similar to the list but the diff is, it does't maintain the sequence
 nom = {5,9,7,9,2}
 nom.add(1)
 print(nom)
