72a5afffa0e6e6c086a838b3b366ab3a64792b53
ARJIT VIJEY <60552906+ARJITVIJEY-GIT@users.noreply.github.com>
2020-05-26 08:24:25 -0700

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,6 @@
+COVID = 'An infectious disease caused by a newly discovered coronavirus.'
+originated_from = 'china'
+symptom=('respiratory problems')
+print('COVID: '+ COVID)
+print('Originated from: '+ originated_from)
+print('symptom: '+ symptom) 
