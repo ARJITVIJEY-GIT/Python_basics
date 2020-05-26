2892b322d4a33656f5579dc517116a667981bf67
ARJIT VIJEY <60552906+ARJITVIJEY-GIT@users.noreply.github.com>
2020-05-26 09:23:48 -0700

    Create list.py

---
+++ list.py
@@ -0,0 +1,11 @@
+nums = [2,8,5,9]
+Items = ['avocado','apple','orange','grape']
+print(nums + Items)
+print(nums[3])
+nums.append(10)
+print(nums)
+nums.sort()
+print(nums)
+Items.pop(2)
+print(Items)
+print(nums + Items)
