4ccae29b9ff873b3a3f415405274f28dd7151ae8
ARJIT VIJEY <60552906+ARJITVIJEY-GIT@users.noreply.github.com>
2020-05-26 10:14:14 -0700

    Update tuplesandsets.py

--- tuplesandsets.py
+++ tuplesandsets.py
@@ -1,8 +1,8 @@
-#tupples,immuttable
+#tuples,immuttable,Tuples are sequences, just like lists. The differences between tuples and lists are, the tuples cannot be changed unlike lists. uses () 
 tup = (3,8,9,5,9,8)
 tup.count(9)
 print(tup)
-#set,similar to the list but the diff is, it does't maintain the sequence
+#set,similar to the list but the diff is, it does't maintain the sequence, unordered collection. uses {}
 nom = {5,9,7,9,2}
 nom.add(1)
 print(nom)
