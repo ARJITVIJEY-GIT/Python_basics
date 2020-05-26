9c2e416780acc5d733ba0b2fbbcd4666fdb20ba8
ARJIT VIJEY <60552906+ARJITVIJEY-GIT@users.noreply.github.com>
2020-05-26 10:10:16 -0700

    Update list.py

--- list.py
+++ list.py
@@ -1,4 +1,4 @@
-#list are mutable(can change.,add,remove)
+#A list is a data structure in Python that are mutable(can change.,add,remove),sequence order, need to use []
 nums = [2,8,5,9]
 Items = ['avocado','apple','orange','grape']
 print(nums + Items)
