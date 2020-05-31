41d9a7412aa637419b973b5df2076aab227f7fb3
ARJIT VIJEY <60552906+ARJITVIJEY-GIT@users.noreply.github.com>
2020-05-31 11:22:37 -0700

    Create number_system_conversion.py

---
+++ number_system_conversion.py
@@ -0,0 +1,23 @@
+#Number system conversion
+# Binary, Decimal, Octal, Hexadecimal
+
+bin(20)  #output : 0b10100 decimal to binary :: bin() used to convert. The conversion result is 10100 so the fisrt two values '0b' is used to represent that it's a binary value
+
+0b1011011 #output : 92 binary to decimal
+
+oct(15)  #output : 0o17 decimal to octal :: answer is 17 and the "0o" represent that it is an Octal
+
+hex(120) #output : 0x78
+
+0x93     #output : 147
+0xf      #output : 15
+
+
+
+
+
+
+
+
+
+
