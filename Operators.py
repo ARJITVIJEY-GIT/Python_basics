0555ba0469f302a2cb1ad9c34cbbc18f0e6974c4
ARJIT VIJEY <60552906+ARJITVIJEY-GIT@users.noreply.github.com>
2020-05-31 10:51:43 -0700

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,42 @@
+#operators
+#Arithmetic, Assignment, Relational, Logical, Unary
+
+#Arithmetic (+,-,*,/,%)
+x = 3
+y = 6
+x + y             #output : 9
+x - y             #output :-3
+
+#Assignment (=)
+x = 2
+x += 2            #means x = x + 2
+x                 #output : 4
+x *= 4
+x                 #output : 16
+a,b = 4,6
+a                 #output : 4
+b                 #output : 6
+
+#Relational (<,>,<=,>=.==,!=)
+x = 6
+y = 8
+x < y            #output : True
+x == y           #output : false
+x != y           #output : True
+x <= y           #output : True
+
+#Logical (AND, OR, NOT)
+a = 9
+b = 4
+a < 10 and b < 2  #output : False
+a < 10 or  b < 2  #output : True
+x = True
+x                 #output : True
+not x             #output : False
+x = True
+x = not x
+x                 #output : False
+
+
+
+
